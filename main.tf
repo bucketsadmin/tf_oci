817097b4287b2fc4373885ea87070dba3a74fca9
bucketsadmin <jsimpsonjr@gmail.com>
2024-12-21 10:50:26 -0600

    thinking about how to map this all out

--- main.tf
+++ main.tf
@@ -5,4 +5,18 @@
 locals {
   domain            = "jjsimpson"
   oci_core_vcn_cidr = "10.0.40.0/22"
+
+  vcn_networks = {
+    primary = {
+      name   = ""
+      domain = ""
+      subnets = {
+        sub_01 = {
+          network = cidrsubnet(local.oci_core_vcn_cidr, 8, 0)
+          dns = ""
+          name = ""
+        }
+      }
+    }
+  }
 }
