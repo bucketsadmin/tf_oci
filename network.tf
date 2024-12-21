4d898189ebe4ff71ec0b2ab61d67fb4892237fa7
bucketsadmin <jsimpsonjr@gmail.com>
2024-12-21 10:50:16 -0600

    added route and gateway

--- network.tf
+++ network.tf
@@ -16,7 +16,20 @@ resource "oci_core_subnet" "default_oci_core_subnet_01" {
   cidr_block     = "10.0.40.0/24"
   compartment_id = data.onepassword_item.oci_iad_compartment_id.password
   display_name   = "oci_jjsimpson_vcn (default) OCI core subnet"
-  # dns_label      = "sub_01.${local.domain}"
+  dns_label      = "sub01"
+  route_table_id = oci_core_vcn.default_oci_core_vcn.default_route_table_id
+  vcn_id         = oci_core_vcn.default_oci_core_vcn.id
+  # security_list_ids = [oci_core_default_security_list.default_security_list.id]
+  freeform_tags = {
+    "provisioner" = "terraform"
+  }
+}
+resource "oci_core_subnet" "default_oci_core_subnet_02" {
+  depends_on     = [oci_core_vcn.default_oci_core_vcn]
+  cidr_block     = "10.0.41.0/24"
+  compartment_id = data.onepassword_item.oci_iad_compartment_id.password
+  display_name   = "oci_jjsimpson_vcn (default) OCI core subnet"
+  dns_label      = "sub02"
   route_table_id = oci_core_vcn.default_oci_core_vcn.default_route_table_id
   vcn_id         = oci_core_vcn.default_oci_core_vcn.id
   # security_list_ids = [oci_core_default_security_list.default_security_list.id]
@@ -25,39 +38,22 @@ resource "oci_core_subnet" "default_oci_core_subnet_01" {
   }
 }
 
-# resource "oci_core_subnet" "oci_core_subnet11" {
-#   cidr_block        = var.oci_core_subnet_cidr11
-#   compartment_id    = var.compartment_ocid
-#   display_name      = "${var.oci_core_subnet_cidr11} OCI core subnet"
-#   dns_label         = var.oci_core_subnet_dns_label11
-#   route_table_id    = oci_core_vcn.default_oci_core_vcn.default_route_table_id
-#   vcn_id            = oci_core_vcn.default_oci_core_vcn.id
-#   security_list_ids = [oci_core_default_security_list.default_security_list.id]
-#   freeform_tags = {
-#     "provisioner"        = "terraform"
-#     "environment"        = var.environment
-#     (var.unique_tag_key) = var.unique_tag_value
-#   }
-# }
-#
-#
-# resource "oci_core_internet_gateway" "default_oci_core_internet_gateway" {
-#   compartment_id = var.compartment_ocid
-#   display_name   = "Internet Gateway Default OCI core vcn"
-#   enabled        = "true"
-#   vcn_id         = oci_core_vcn.default_oci_core_vcn.id
-#   freeform_tags = {
-#     "provisioner"        = "terraform"
-#     "environment"        = var.environment
-#     (var.unique_tag_key) = var.unique_tag_value
-#   }
-# }
-#
-# resource "oci_core_default_route_table" "default_oci_core_default_route_table" {
-#   route_rules {
-#     destination       = "0.0.0.0/0"
-#     destination_type  = "CIDR_BLOCK"
-#     network_entity_id = oci_core_internet_gateway.default_oci_core_internet_gateway.id
-#   }
-#   manage_default_resource_id = oci_core_vcn.default_oci_core_vcn.default_route_table_id
-# }
+resource "oci_core_default_route_table" "default_oci_core_default_route_table" {
+  route_rules {
+    description       = "tf_route_table"
+    destination       = "0.0.0.0/0"
+    destination_type  = "CIDR_BLOCK"
+    network_entity_id = oci_core_internet_gateway.default_oci_core_internet_gateway.id
+  }
+  manage_default_resource_id = oci_core_vcn.default_oci_core_vcn.default_route_table_id
+}
+
+resource "oci_core_internet_gateway" "default_oci_core_internet_gateway" {
+  compartment_id = data.onepassword_item.oci_iad_compartment_id.password
+  display_name   = "Internet Gateway Default OCI core vcn"
+  enabled        = "true"
+  vcn_id         = oci_core_vcn.default_oci_core_vcn.id
+  freeform_tags = {
+    "provisioner" = "terraform"
+  }
+}
