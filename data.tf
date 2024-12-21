39aeb6bee327738c4ec835b10d4f0e324afd5d85
bucketsadmin <jsimpsonjr@gmail.com>
2024-12-21 10:32:04 -0600

    add compartment id

--- data.tf
+++ data.tf
@@ -4,7 +4,6 @@
 data "onepassword_vault" "secrets" {
   name = "secrets"
 }
-
 data "onepassword_item" "oci_iad_tenancy_oid" {
   vault = replace(data.onepassword_vault.secrets.id, "vaults/", "")
   title = "oci_iad_tenancy_oid"
@@ -20,4 +19,14 @@ data "onepassword_item" "oci_iad_tfuser_fingerprint" {
 data "onepassword_item" "oci_iad_tfuser_key" {
   vault = replace(data.onepassword_vault.secrets.id, "vaults/", "")
   title = "oci_iad_tfuser_key"
+}
+data "onepassword_item" "oci_iad_compartment_id" {
+  vault = replace(data.onepassword_vault.secrets.id, "vaults/", "")
+  title = "oci_iad_compartment_id"
+}
+
+###### OCI
+data "oci_identity_availability_domain" "ad" {
+  compartment_id = data.onepassword_item.oci_iad_compartment_id.password
+  ad_number      = 1
 }
