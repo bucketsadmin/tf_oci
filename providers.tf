7e44297eda9757eee1f162a27b2dde8d0c482a51
JJ Simpson <jsimpsonjr@gmail.com>
2025-02-07 15:25:17 -0600

    use latest oci version

--- providers.tf
+++ providers.tf
@@ -7,7 +7,7 @@ terraform {
   required_providers {
     oci = {
       source  = "oracle/oci"
-      version = "~> 6.21"
+      version = "~> 6.25"
     }
     onepassword = {
       source  = "1password/onepassword"
